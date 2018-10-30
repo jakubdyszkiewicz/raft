55fedba0bc088356953c1b263c5ccd589c0473a0
Jakub Dyszkiewicz <jakub.dyszkiewicz@allegro.pl>
2018-10-30 10:22:32 +0100

    Refactored handlers to server

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	".."
-	raftHttp "../http"
-	"log"
-	"net/http"
+	"../http"
 	"os"
 )
 
@@ -14,19 +12,8 @@ func main() {
 		panic("Pass NodeID as a first arg and peers as a next")
 	}
 
-	client := raftHttp.NewClient()
+	client := http.NewClient()
 	r := raft.NewRaft(client.SendRequestVote, client.SendAppendEntries, args[0], args[1:])
-	handlers := raftHttp.NewHandlers(r)
-
-	http.HandleFunc("/raft/state", handlers.HandleState)
-	http.HandleFunc("/raft/request-vote", handlers.HandleRequestVote)
-	http.HandleFunc("/raft/append-entries", handlers.HandleAppendEntries)
-	log.Fatal(http.ListenAndServe(args[0], logRequest(http.DefaultServeMux)))
-}
-
-func logRequest(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
-		handler.ServeHTTP(w, r)
-	})
+	server := http.NewServer(r)
+	server.Start(args[0])
 }
---
+++ http/server.go
@@ -0,0 +1,89 @@
+package http
+
+import (
+	".."
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Server struct {
+	raft *raft.Raft
+}
+
+func NewServer(raft *raft.Raft) *Server {
+	return &Server{
+		raft: raft,
+	}
+}
+
+func (s *Server) Start(listenAddr string) {
+	http.HandleFunc("/raft/state", s.HandleState)
+	http.HandleFunc("/raft/request-vote", s.HandleRequestVote)
+	http.HandleFunc("/raft/append-entries", s.HandleAppendEntries)
+	log.Fatal(http.ListenAndServe(listenAddr, logRequest(http.DefaultServeMux)))
+}
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func (s *Server) HandleState(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("Access-Control-Allow-Origin", "*")
+	res := s.raft.State()
+	writeJson(res, writer)
+}
+
+type AppendEntriesRequest struct {
+	Term int `json:"term"`
+}
+
+type AppendEntriesResponse struct {
+	Term int `json:"term"`
+	Success bool `json:"success"`
+}
+
+func (s *Server) HandleAppendEntries(writer http.ResponseWriter, request *http.Request) {
+	appendEntriesRequest := AppendEntriesRequest{}
+	readJson(appendEntriesRequest, request)
+
+	success := s.raft.AppendEntries(appendEntriesRequest.Term)
+
+	appendEntriesResponse := AppendEntriesResponse{Term:s.raft.State().CurrentTerm, Success:success}
+	writeJson(appendEntriesResponse, writer)
+}
+
+type RequestVoteRequest struct {
+	Term int `json:"term"`
+	CandidateId string `json:"candidateId"`
+}
+
+type RequestVoteResponse struct {
+	Term int `json:"term"`
+	VoteGranted bool `json:"voteGranted"`
+}
+
+func (s *Server) HandleRequestVote(writer http.ResponseWriter, request *http.Request) {
+	voteRequest := RequestVoteRequest{}
+	readJson(voteRequest, request)
+
+	voteGranted := s.raft.RequestVote(voteRequest.Term, voteRequest.CandidateId)
+
+	voteResponse := RequestVoteResponse{VoteGranted:voteGranted, Term:s.raft.State().CurrentTerm}
+	writeJson(voteResponse, writer)
+}
+
+func readJson(obj interface{}, request *http.Request) {
+	body, _ := ioutil.ReadAll(request.Body)
+	json.Unmarshal(body, obj)
+}
+
+func writeJson(obj interface{}, writer http.ResponseWriter) {
+	b, _ := json.Marshal(obj)
+	writer.Write(b)
+	writer.Header().Add("content-type", "application/json")
+}
